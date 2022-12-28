733164001821338ed3bed4d809ae85a26e9a7a75
Jayanta <bjayanta.me@gmail.com>
2022-12-28 22:59:24 +0600

    update repo

--- js/aside.js
+++
@@ -1,7 +0,0 @@
-function asideToggle(event) {
-    if (event != undefined) {
-        event.classList.toggle('active');
-    }
-    
-    document.getElementById('wrapper').classList.toggle("aside-open");
-}
